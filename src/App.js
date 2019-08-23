25c23ff5c1bd7a687082e34b0b86a74f188fb32b
josepaniagua <jpanjr@gmail.com>
2019-08-23 15:47:53 -0700

    added data to state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { numbers, operators, specials } from './data.js'
 // STEP 4 - import the button and display components
@@ -13,6 +13,9 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+    const [numState, setNumState] = useState(numbers);
+    const [operState, setOperState] = useState(operators);
+    const [specState, setSpecState] = useState(specials);
 
   return (
     <div className="container">
